4e86ae429dd0190e285df54d88c6dd97a32f7893
Foster Clark <fosterclark48@yahoo.com>
2023-01-09 12:39:09 -0600

    working through pep8 styling

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -3,10 +3,10 @@ import random
 number = random.randint(-10000, 10000)
 if number > 5:
     print("Last digit of {:d} is {:s} and is greater than 5".format(number,
-                str(number)[-1]))
+                                                                    str(number)[-1]))
 elif number == 0:
     print("Last digit of {:d} is {:s} and is 0".format(number,
-                str(number)[-1]))
+                                                                    str(number)[-1]))
 elif number < 6 and not 0:
     if number < 0:
         print("Last digit of {:d} is -{:s} and is less than 6 and not 0".format(
