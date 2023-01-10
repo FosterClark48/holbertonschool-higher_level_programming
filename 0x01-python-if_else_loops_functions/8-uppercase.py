09aa0f34c8875072720b01b7dda9bef4376773ed
Foster Clark <fosterclark48@yahoo.com>
2023-01-09 22:16:59 -0600

    updated code to print all uppercase chars

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = ''
     for char in str:
-        if(ord(char) >= 97 and ord(char) <= 122):
-            x = ord(char) - 32
-            print(x)
+        if(ord(char) >= 65):
+            result += char(ord(char) - 32)
+    return result
+print(str)
