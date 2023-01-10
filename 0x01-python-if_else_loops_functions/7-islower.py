1614f669b6dcd9002473e6c6f12d734ad3af2feb
Foster Clark <fosterclark48@yahoo.com>
2023-01-10 13:12:26 -0600

    fixed edge case

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-    for char in c:
-        if(ord(char) >= 97 and ord(char) <= 122):
-            return True
-        return False
+    if c:
+        for char in c:
+            if(ord(char) >= 97 and ord(char) <= 122):
+                return True
+            return False
