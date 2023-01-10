c8a2ec7e56a2dc8075ab838f15133a8076c99acd
Foster Clark <fosterclark48@yahoo.com>
2023-01-10 16:23:59 -0600

    stored len(sys.argv) in a variable and subtracted 1 from it

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys
+arg = len(sys.argv) - 1
 if len(sys.argv) == 0:
     print("{:d} arguments.".format(len(sys.argv)))
 elif len(sys.argv) == 1:
     print("{:d} argument:".format(len(sys.argv)))
 else:
     print("{:d} arguments:".format(len(sys.argv)))
-for i in range(len(sys.argv)):
+for i in range(arg):
     print("{:d}: {} ".format(i + 1, sys.argv[i + 1]))
