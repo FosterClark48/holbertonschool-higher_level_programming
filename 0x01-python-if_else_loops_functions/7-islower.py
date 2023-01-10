10788280a47ba8cd625a4de95caa768405a54dcf
Foster Clark <fosterclark48@yahoo.com>
2023-01-09 21:29:12 -0600

    changed to use islower built in function

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
-    ctr = 0
-    for char in c:
-        if(ord(char) >= 97 and ord(char) <= 122):
-            ctr = ctr + 1
-    if(ctr > 0):
+    if c.islower():
         return True
+    return False
