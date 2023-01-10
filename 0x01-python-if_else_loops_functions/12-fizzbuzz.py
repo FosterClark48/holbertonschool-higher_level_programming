45b98d838f26794f2d4085d8b74bcdc70c4c38e8
Foster Clark <fosterclark48@yahoo.com>
2023-01-09 23:51:51 -0600

    missed a new line change for line 11

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,13 +2,13 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 15 == 0:
-            print("FizzBuzz", end=" ")
+            print("FizzBuzz", end="")
         elif (i % 3) == 0:
-            print("Fizz", end=" ")
+            print("Fizz", end="")
         elif (i % 5) == 0:
-            print("Buzz", end=" ")
+            print("Buzz", end="")
         else:
-            print("{}".format(i))
+            print("{}".format(i), end="")
         if i < 100:
-            print(" ", end=" ")
-    print(end=" ")
+            print(" ", end="")
+    print(end="")
