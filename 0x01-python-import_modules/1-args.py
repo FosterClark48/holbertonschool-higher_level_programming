df6554118092602c7244e26a6a5f8209cf879af7
Foster Clark <fosterclark48@yahoo.com>
2023-01-10 16:25:15 -0600

    added execute guard

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-arg = len(sys.argv) - 1
-if len(sys.argv) == 0:
-    print("{:d} arguments.".format(len(sys.argv)))
-elif len(sys.argv) == 1:
-    print("{:d} argument:".format(len(sys.argv)))
-else:
-    print("{:d} arguments:".format(len(sys.argv)))
-for i in range(arg):
-    print("{:d}: {} ".format(i + 1, sys.argv[i + 1]))
+if __name__ == "__main__":
+    import sys
+    arg = len(sys.argv) - 1
+    if len(sys.argv) == 0:
+        print("{:d} arguments.".format(len(sys.argv)))
+    elif len(sys.argv) == 1:
+        print("{:d} argument:".format(len(sys.argv)))
+    else:
+        print("{:d} arguments:".format(len(sys.argv)))
+    for i in range(arg):
+        print("{:d}: {} ".format(i + 1, sys.argv[i + 1]))
