56cc3cb7bea758ff74c9e4fc1ad49c7cb2181c37
Foster Clark <fosterclark48@yahoo.com>
2023-01-09 23:00:35 -0600

    added print() for new line at end

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    result = ""
     for char in str:
         if ord(char) >= ord('a') and ord(char) <= ord('z'):
             char = chr(ord(char) - 32)
         print("{:s}".format(char), end="")
+    print()
