59524a35456799d3c9f07db2efe375dd2bf3c36d
Foster Clark <fosterclark48@yahoo.com>
2023-01-10 16:17:18 -0600

    testing

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -2,9 +2,9 @@
 import sys
 if len(sys.argv) == 0:
     print("{:d} arguments.".format(len(sys.argv)))
-if len(sys.argv) == 1:
+elif len(sys.argv) == 1:
     print("{:d} argument:".format(len(sys.argv)))
 else:
     print("{:d} arguments:".format(len(sys.argv)))
 for i in range(len(sys.argv)):
-    print("{:d}: {:s} ".format(i + 1, sys.argv[i + 1]))
+    print("{:d}: {} ".format(i + 1, sys.argv[i + 1]))
