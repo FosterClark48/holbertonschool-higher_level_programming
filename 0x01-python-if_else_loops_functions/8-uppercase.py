11064affde9cddd3159eefb9a8c8d9261b16ae61
Foster Clark <fosterclark48@yahoo.com>
2023-01-09 22:59:35 -0600

    changed ord ints to chars

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     result = ""
     for char in str:
-        if ord('a') >= ord(97) and ord(char) <= ord('z'):
+        if ord(char) >= ord('a') and ord(char) <= ord('z'):
             char = chr(ord(char) - 32)
         print("{:s}".format(char), end="")
