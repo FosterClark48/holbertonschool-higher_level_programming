2493866a75b9051dc049c6c8dedc87c5ed207d64
Foster Clark <fosterclark48@yahoo.com>
2023-01-10 16:27:25 -0600

    switched out arg in .format for variable

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     import sys
     arg = len(sys.argv) - 1
     if len(sys.argv) == 0:
-        print("{:d} arguments.".format(len(sys.argv)))
+        print("{:d} arguments.".format(arg))
     elif len(sys.argv) == 1:
-        print("{:d} argument:".format(len(sys.argv)))
+        print("{:d} argument:".format(arg))
     else:
-        print("{:d} arguments:".format(len(sys.argv)))
+        print("{:d} arguments:".format(arg))
     for i in range(arg):
         print("{:d}: {} ".format(i + 1, sys.argv[i + 1]))
