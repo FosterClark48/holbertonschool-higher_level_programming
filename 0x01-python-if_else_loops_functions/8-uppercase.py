aa21132e2a1a4bbf6a87f4a6038c6545ad431427
Foster Clark <fosterclark48@yahoo.com>
2023-01-09 22:58:14 -0600

    chagned ord ints to chars

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     result = ""
     for char in str:
-        if ord(char) >= ord(97) and ord(char) <= ord(122):
+        if ord('a') >= ord(97) and ord(char) <= ord('z'):
             char = chr(ord(char) - 32)
         print("{:s}".format(char), end="")
