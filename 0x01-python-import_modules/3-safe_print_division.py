69ef97fb46ae80b39e52b75e532bb25897304026
Foster Clark <fosterclark48@yahoo.com>
2023-01-10 20:55:07 -0600

    removed exception case

--- 0x01-python-import_modules/3-safe_print_division.py
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     quo = "Inside Result"
     try:
         res = a / b
-    except ZeroDivisionError:
+    except:
         print("{}: None".format(quo))
     finally:
         print("{}: {}".format(quo, res))
