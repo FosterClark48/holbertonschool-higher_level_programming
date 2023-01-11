1f81d004a2c0928111efb4d00b84112a1d27424d
Foster Clark <fosterclark48@yahoo.com>
2023-01-10 20:32:17 -0600

    function that divides 2 integers and prints the result.

---
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    quo = "Inside Result"
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("{:s}: None".format(quo))
+    finally:
+        print("{:s}: {:d}".format(quo, result))
