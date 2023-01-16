1a2a65e890266eda26df2612972eb6d861a446da
Foster Clark <fosterclark48@yahoo.com>
2023-01-15 21:15:45 -0600

    added '=' to assignment op line 6

--- 0x02-python-data_structures/1-element_at.py
+++ 0x02-python-data_structures/1-element_at.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     i = my_list[idx]
-    print(len(my_list))
     if i < 0:
         return None
-    if i > len(my_list):
+    if i >= len(my_list):
         return None
     else:
         return i
