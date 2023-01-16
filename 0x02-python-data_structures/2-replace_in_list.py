b05616444a9b59c8b4f4819fb89dd1e4bbce6482
Foster Clark <fosterclark48@yahoo.com>
2023-01-16 14:31:45 -0600

    changed variable i to idx

--- 0x02-python-data_structures/2-replace_in_list.py
+++ 0x02-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    for i in range(len(my_list)):
-        if my_list[i] == element:
-            my_list[i] = element
-    if idx >= 0 and len(my_list) > idx:
-        return my_list[idx]
-    return 'None'
-    
+    for idx in range(len(my_list)):
+        if my_list[idx] == element:
+            my_list[idx] = element
+    if idx > 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
