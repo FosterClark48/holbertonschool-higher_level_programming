62482dde8f5249972d1247c44291bcd86ed4252e
Foster Clark <fosterclark48@yahoo.com>
2023-01-15 21:07:13 -0600

    changed var names in if statements

--- 0x02-python-data_structures/1-element_at.py
+++ 0x02-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     i = my_list[idx]
-    if idx < my_list[0]:
+    if i < 0:
         return None
-    if idx > len(my_list):
+    if i > len(my_list):
         return None
     else:
         return i
