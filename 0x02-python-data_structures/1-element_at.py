f05013216b5269f24271a2179917f9bdf6eb4442
Foster Clark <fosterclark48@yahoo.com>
2023-01-16 14:14:49 -0600

    changed assignment ops

--- 0x02-python-data_structures/1-element_at.py
+++ 0x02-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     i = my_list[idx]
-    if idx >= 0 and len(my_list) > idx:
-        return i
-    return 'None'
+    if idx > 0 or idx >= len(my_list):
+        return None
+    return i
