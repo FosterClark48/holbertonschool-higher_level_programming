647814782ed19f489ad5f56aeb0ce92438d8d4af
Foster Clark <fosterclark48@yahoo.com>
2023-01-16 14:49:20 -0600

    assigned my_list[idx] to arg: 'element'

--- 0x02-python-data_structures/2-replace_in_list.py
+++ 0x02-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if my_list:
-        for idx in range(len(my_list)):
-            if my_list[idx] == element:
-                my_list[idx] = element
+        my_list[idx] = element
         if idx < 0 or idx >= len(my_list):
             return None
-        return my_list[idx]
+        return my_list
