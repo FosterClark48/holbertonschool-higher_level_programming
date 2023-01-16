d31895b8e98558f0df41c971297696a1070ca54d
Foster Clark <fosterclark48@yahoo.com>
2023-01-15 21:05:15 -0600

    Testing using a var

--- 0x02-python-data_structures/1-element_at.py
+++ 0x02-python-data_structures/1-element_at.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    i = my_list[idx]
     if idx < my_list[0]:
         return None
     if idx > len(my_list):
         return None
     else:
-        return idx
+        return i
