7f5c679496d5229fd4b717aa1feb8a044f7ccf7c
Foster Clark <fosterclark48@yahoo.com>
2023-01-15 21:25:15 -0600

    changed assignemnt op on line 6

--- 0x02-python-data_structures/1-element_at.py
+++ 0x02-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     i = my_list[idx]
     if i < 0:
         return None
-    elif len(my_list) > i:
+    elif len(my_list) < i:
         return None
     else:
         return i
