eeffe106979fb1c8c31d498e5998551a6c6edbed
Foster Clark <fosterclark48@yahoo.com>
2023-01-16 16:04:55 -0600

    Function that removes chars 'c' & 'C' using translate() method

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if my_string:
-        for i in range(len(my_string)):
-            if my_string[i] == 67 or my_string[i] == 99:
-                continue
-            else:
-                return my_string
+        my_string.translate({ord('c'): None}, {ord('C'): None})
+        return my_string
