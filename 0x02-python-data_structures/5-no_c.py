6085419fe9d21e6c25d40a20ca25cee7fe1b35c2
Foster Clark <fosterclark48@yahoo.com>
2023-01-16 15:56:01 -0600

    changed var i to specify index of my_string

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     if my_string:
         for i in range(len(my_string)):
-            if i == 67 or i == 99:
+            if my_string[i] == 67 or my_string[i] == 99:
                 continue
             else:
                 return my_string
