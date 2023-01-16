2eefc2ce50c076d738431572b1b4574eac8aa41c
Foster Clark <fosterclark48@yahoo.com>
2023-01-16 16:34:49 -0600

    removed args and 2 replacement fields

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix:
-        print("{} {} {}".format(matrix, matrix, matrix))
+        print("{}".format(matrix))
     else:
         print(matrix)
