7e16efeb1a6619a5c5a7eae09d4b71badbd0a56f
Foster Clark <fosterclark48@yahoo.com>
2023-01-15 21:14:42 -0600

    fixing test

--- 0x02-python-data_structures/1-element_at.py
+++ 0x02-python-data_structures/1-element_at.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     i = my_list[idx]
+    print(len(my_list))
     if i < 0:
         return None
     if i > len(my_list):
         return None
     else:
         return i
-        print(len(my_list))
