5021ffcb8946e01c7a9baed381921a884efc9295
Foster Clark <fosterclark48@yahoo.com>
2023-01-15 21:21:01 -0600

    changed it back

--- 0x02-python-data_structures/1-element_at.py
+++ 0x02-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     i = my_list[idx]
     if i < 0:
         return None
-    if i >= len(my_list):
+    if i > len(my_list):
         return None
     else:
         return i
