28348a775ae568f63c1932e11fef4d596830afec
Foster Clark <fosterclark48@yahoo.com>
2023-01-15 21:02:31 -0600

    simplifying retrieving an element

--- 0x02-python-data_structures/1-element_at.py
+++ 0x02-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for idx in my_list:
-        if idx < my_list[0]:
-            print("None")
-        if idx > len(my_list):
-            print("None")
-        print("{:d}".format(idx))
+    if idx < my_list[0]:
+        print("None")
+    if idx > len(my_list):
+        print("None")
+    else:
+        print(idx)
