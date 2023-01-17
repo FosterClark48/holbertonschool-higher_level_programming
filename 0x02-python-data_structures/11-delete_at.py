6b191192b7fc84f943733a7bdf6b322cb781dde9
Foster Clark <fosterclark48@yahoo.com>
2023-01-17 14:24:47 -0600

    testing output

--- 0x02-python-data_structures/11-delete_at.py
+++ 0x02-python-data_structures/11-delete_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if my_list:
-        if idx < len(my_list) or idx > len(my_list):
-            return my_list
+        #if idx < len(my_list) or idx > len(my_list):
+            #return my_list
         my_list.remove(idx)
-    return my_list[idx]
+        print(my_list)
+    #return my_list[idx]
