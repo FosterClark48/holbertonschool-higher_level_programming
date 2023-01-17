0615b5eda56aececdf0b51fbb952304e8338f53e
Foster Clark <fosterclark48@yahoo.com>
2023-01-16 22:04:58 -0600

    added another list comprehension to check for edge case less than 0

--- 0x02-python-data_structures/7-add_tuple.py
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    for i, t in enumerate(tuple_a):
-        if i < 2:
-            tuple_a[i] = (0, t[1])
+    tuple_a = [(1, 89)]
+    tuple_a = [(x if i > 2 else 0, y) for i, (x, y) in enumerate(tuple_a)]
     tuple_sum = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
     return tuple_sum
