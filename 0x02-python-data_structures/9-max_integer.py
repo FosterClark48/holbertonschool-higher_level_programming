5583899ad74270272a9ae07538ba78af01a36b8c
Foster Clark <fosterclark48@yahoo.com>
2023-01-17 13:17:20 -0600

    added if statement for edge case

--- 0x02-python-data_structures/9-max_integer.py
+++ 0x02-python-data_structures/9-max_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = sorted(my_list)[-1]
-    return max
+    if my_list:
+        max = sorted(my_list)[-1]
+        return max
+    return None
