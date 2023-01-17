6cf66150f64a8c2afd6e51b8e89506df417b521a
Foster Clark <fosterclark48@yahoo.com>
2023-01-16 20:44:22 -0600

    removed the incorrect 'start' arg for sum()

--- 0x02-python-data_structures/7-add_tuple.py
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     # Adds 2 tuples
-    tuple_sum = sum(tuple_a, tuple_b)
+    tuple_sum = sum(tuple_a)
     # Returns tuple with 2 ints
     return tuple_sum
