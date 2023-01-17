55220c8a4e3f862dead87efa51ce5144cb3888d3
Foster Clark <fosterclark48@yahoo.com>
2023-01-17 11:49:38 -0600

    turned tuple to str and finding first letter of str with indexing

--- 0x02-python-data_structures/8-multiple_returns.py
+++ 0x02-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    leng = len(sentence)
-    for i in str(leng):
-        #if i == 0:
-            #return None
-        print(leng)
+    str = ''.join(map(str, sentence))
+    leng = len(str)
+    fir = str[0]
+    for i in leng:
+        if i <= 0:
+            return None
+    return (leng, fir)
