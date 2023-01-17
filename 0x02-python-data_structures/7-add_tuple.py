5aaa5f4eaf2de8175eefbaa6417b811e00c2318b
Foster Clark <fosterclark48@yahoo.com>
2023-01-16 22:05:35 -0600

    changing back to what i had before

--- 0x02-python-data_structures/7-add_tuple.py
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a = [(1, 89)]
-    tuple_a = [(x if i > 2 else 0, y) for i, (x, y) in enumerate(tuple_a)]
     tuple_sum = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
     return tuple_sum
