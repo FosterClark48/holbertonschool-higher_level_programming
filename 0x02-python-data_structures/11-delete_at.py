9925c2bd1ee31b633a458263b4411bf40c0c94c8
Foster Clark <fosterclark48@yahoo.com>
2023-01-17 14:27:55 -0600

    changed remove() to del method

--- 0x02-python-data_structures/11-delete_at.py
+++ 0x02-python-data_structures/11-delete_at.py
@@ -3,5 +3,5 @@ def delete_at(my_list=[], idx=0):
     if my_list:
         #if idx < len(my_list) or idx > len(my_list):
             #return my_list
-        my_list.remove(idx)
+        del my_list[idx]
     return my_list
