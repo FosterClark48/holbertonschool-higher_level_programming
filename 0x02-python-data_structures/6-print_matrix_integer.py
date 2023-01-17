0874a5774b6543bf726692fd190a4ad4c230f0d3
Foster Clark <fosterclark48@yahoo.com>
2023-01-16 19:08:11 -0600

    added list comprehension after str.format()

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix:
-        for i in matrix:
-            print(" ".join("{:d}".format(i)))
+        for row in matrix:
+            print(" ".join("{:d}".format(column) for column in row))
