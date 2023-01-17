5c2e347d29978ea7371f863ee9da87a9d2f17953
Foster Clark <fosterclark48@yahoo.com>
2023-01-16 21:34:53 -0600

    testing to see what continue does

--- 0x02-python-data_structures/7-add_tuple.py
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -2,6 +2,6 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     for i in tuple_a and tuple_b:
         if i < 2:
-            return 0
+            continue
     tuple_sum = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
     return tuple_sum
