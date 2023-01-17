330b9006d9d3701663f0c10f21fc1ab3eaeffc7c
Foster Clark <fosterclark48@yahoo.com>
2023-01-16 21:23:25 -0600

    Using map() + lambda to add tuple elements together

--- 0x02-python-data_structures/7-add_tuple.py
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_sum = ()
-    for i in tuple_a:
-        tuple_sum.append(tuple_a[i] + tuple_b[i])
+    tuple_sum = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
     return tuple_sum
