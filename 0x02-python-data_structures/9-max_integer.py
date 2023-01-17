bff66e02cd6d6835ad1e9b163d00fadd8a27321a
Foster Clark <fosterclark48@yahoo.com>
2023-01-17 13:07:00 -0600

    passed 'max' to range()

--- 0x02-python-data_structures/9-max_integer.py
+++ 0x02-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     max = my_list[0]
     if max:
-        for i in max:
+        for i in range(max):
             if i > max:
                 max = i
         return max
