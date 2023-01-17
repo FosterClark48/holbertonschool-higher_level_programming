6a6e1dca3c0a998ba9f0e83e2420765f7b841382
Foster Clark <fosterclark48@yahoo.com>
2023-01-17 13:42:38 -0600

    testing out this code for even numbers in a list

--- 0x02-python-data_structures/10-divisible_by_2.py
+++ 0x02-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     if my_list:
-        for num in range(ord(my_list)):
-            if num % 2 == 0:
+        newList = [my_list.copy]
+        for i in range(newList):
+            if i % 2 == 0:
                 return True
     return False
