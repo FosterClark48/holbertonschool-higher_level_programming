5fe4f2517d9b6902eb437731b50f7f8246f5d5af
Foster Clark <fosterclark48@yahoo.com>
2023-01-17 11:54:21 -0600

    changed leng on line 6 to range()

--- 0x02-python-data_structures/8-multiple_returns.py
+++ 0x02-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@ def multiple_returns(sentence):
     st = ''.join(map(str, sentence))
     leng = len(st)
     fir = st[0]
-    for i in leng:
+    for i in range(leng):
         if i <= 0:
             return None
-    return (leng, fir)
+    return (st, fir)
