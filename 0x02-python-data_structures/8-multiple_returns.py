e4322c512c39dbe69a9a5707c53df3af26571454
Foster Clark <fosterclark48@yahoo.com>
2023-01-17 11:16:02 -0600

    changing iterable in for loop to range()

--- 0x02-python-data_structures/8-multiple_returns.py
+++ 0x02-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    for i in len(sentence):
+    for i in range(len(sentence)):
         if i <= 0:
             return None
         return len(sentence)
-    
