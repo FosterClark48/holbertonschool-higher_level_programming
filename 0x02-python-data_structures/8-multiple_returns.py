4d090add3886c65a7a54cae308aea6f30bd61f33
Foster Clark <fosterclark48@yahoo.com>
2023-01-17 11:51:16 -0600

    had to change str var name to st

--- 0x02-python-data_structures/8-multiple_returns.py
+++ 0x02-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    str = ''.join(map(str, sentence))
-    leng = len(str)
-    fir = str[0]
+    st = ''.join(map(str, sentence))
+    leng = len(st)
+    fir = st[0]
     for i in leng:
         if i <= 0:
             return None
