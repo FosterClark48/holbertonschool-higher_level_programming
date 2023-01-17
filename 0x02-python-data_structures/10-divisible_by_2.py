4684ac7bdf9161d6c765637e721d8d22e12f1760
Foster Clark <fosterclark48@yahoo.com>
2023-01-17 13:49:03 -0600

    Function that checks if a number in a list is even or odd

--- 0x02-python-data_structures/10-divisible_by_2.py
+++ 0x02-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if my_list:
-        newList = [my_list.copy]
-        for i in range(newList):
-            if i % 2 == 0:
-                return True
-    return False
+    newList = []
+    for i in my_list:
+        newList.append(i % 2 == 0)
+    return newList
