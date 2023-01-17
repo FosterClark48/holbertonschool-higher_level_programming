134e24551e27da7a63a2be4e4f5c2cee30accb15
Foster Clark <fosterclark48@yahoo.com>
2023-01-17 14:40:00 -0600

    changed if statement to just go from range of 0 to length of my_list

--- 0x02-python-data_structures/11-delete_at.py
+++ 0x02-python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if my_list:
-        if idx < len(my_list) or idx > len(my_list):
+        if idx in range(len(my_list) - 1):
             del my_list[idx]
     return my_list
