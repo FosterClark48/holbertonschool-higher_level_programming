50598855c0fb389763c374eff85b23af62640431
Foster Clark <fosterclark48@yahoo.com>
2023-01-17 14:28:47 -0600

    checking for edge cases

--- 0x02-python-data_structures/11-delete_at.py
+++ 0x02-python-data_structures/11-delete_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if my_list:
-        #if idx < len(my_list) or idx > len(my_list):
-            #return my_list
-        del my_list[idx]
+        if idx < len(my_list) or idx > len(my_list):
+            del my_list[idx]
     return my_list
