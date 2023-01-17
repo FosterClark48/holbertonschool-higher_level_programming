986a68957fff5f6ee2dd651d64585cad95f1e510
Foster Clark <fosterclark48@yahoo.com>
2023-01-17 11:19:30 -0600

    turning iterable into str with str()

--- 0x02-python-data_structures/8-multiple_returns.py
+++ 0x02-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    for i in tuple(length):
+    for i in str(length):
         if i <= 0:
             return None
         return length
