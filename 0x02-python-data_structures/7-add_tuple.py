0c289c7de3b096933a07cfa7edc7601d0614cf3c
Foster Clark <fosterclark48@yahoo.com>
2023-01-17 11:13:29 -0600

    pep8 whitespace after ',' on line 4

--- 0x02-python-data_structures/7-add_tuple.py
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     a = tuple_a or (0, 0)
-    b = tuple_b or (0,0)
+    b = tuple_b or (0, 0)
     if len(tuple_a) == 1:
         a = (tuple_a[0], 0)
     if len(tuple_b) == 1:
