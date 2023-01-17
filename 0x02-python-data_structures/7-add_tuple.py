bbbdb822ac6f60bf525f25036ce0dc6accfe4237
Foster Clark <fosterclark48@yahoo.com>
2023-01-16 21:52:40 -0600

    trying to turn tuple element to 0

--- 0x02-python-data_structures/7-add_tuple.py
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    for i in tuple_a and tuple_b:
+    for i, t in enumerate(tuple_a):
         if i < 2:
-            i = 0
+            tuple_a[i] = (0, t[1])
     tuple_sum = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
     return tuple_sum
