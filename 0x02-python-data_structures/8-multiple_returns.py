e2aa0881d7d0c4073ef488eddd9ddbf981d940d5
Foster Clark <fosterclark48@yahoo.com>
2023-01-17 12:02:47 -0600

    scratched everything and used list comprehension

--- 0x02-python-data_structures/8-multiple_returns.py
+++ 0x02-python-data_structures/8-multiple_returns.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    st = ''.join(map(str, sentence))
-    print(st)
-    leng = len(st)
-    print(leng)
-    fir = st[0]
-    print(fir)
-    for i in range(leng):
-        if i <= 0:
-            return None
-    return (st, fir)
+    return (len(sentence), sentence[0] if sentence else None)
