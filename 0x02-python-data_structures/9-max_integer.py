80d82d62bf6bfc08e6c8a9e204c4f4f47016849c
Foster Clark <fosterclark48@yahoo.com>
2023-01-17 13:14:54 -0600

    Function that finds max int of a list w/out max(). Using sorted() method

--- 0x02-python-data_structures/9-max_integer.py
+++ 0x02-python-data_structures/9-max_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = my_list
-    if max:
-        for i in max:
-            if i > max:
-                max = i
-        return max
+    max = sorted(my_list)[-1]
+    return max
