0dad0b33a7bf881adc41afcab855999a9f4949d8
Foster Clark <fosterclark48@yahoo.com>
2023-01-17 21:34:05 -0600

    Added print method to print '#' in a square given the size

--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -22,3 +22,11 @@ class Square:
 
     def area(self):
         return int(self.__size) ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
