96967bc6a2499f97f5660f5394dac4b45ff9d405
Foster Clark <fosterclark48@yahoo.com>
2023-01-18 11:58:19 -0600

    changed line 15

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -12,7 +12,7 @@ def roman_to_int(roman_string):
     result = 0
     for i in roman_string:
         if i in my_dict:
-            if my_dict[i] < my_dict[i + 1]:
+            if my_dict[i] < my_dict[roman_string[i + 1]]:
                 result -= my_dict[i]
             else:
                 result += my_dict[i]
