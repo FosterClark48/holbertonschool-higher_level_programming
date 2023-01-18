4704648545b039b16c9393b1821d8343d6849344
Foster Clark <fosterclark48@yahoo.com>
2023-01-18 11:55:27 -0600

    added my_dict to roman_string

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -12,7 +12,7 @@ def roman_to_int(roman_string):
     result = 0
     for i in roman_string:
         if i in my_dict:
-            if roman_string[i] < roman_string[i + 1]:
+            if my_dict[roman_string[i]] < my_dict[roman_string[i + 1]]:
                 sum -= my_dict[i]
             else:
                 sum += my_dict[i]
