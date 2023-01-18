92b8dbae015d3f6fa7b505f6b6efa02e108abc14
Foster Clark <fosterclark48@yahoo.com>
2023-01-18 11:57:17 -0600

    removed reoman_string and chagned sum to result

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -12,8 +12,8 @@ def roman_to_int(roman_string):
     result = 0
     for i in roman_string:
         if i in my_dict:
-            if my_dict[roman_string[i]] < my_dict[roman_string[i + 1]]:
-                sum -= my_dict[i]
+            if my_dict[i] < my_dict[i + 1]:
+                result -= my_dict[i]
             else:
-                sum += my_dict[i]
+                result += my_dict[i]
     return result
