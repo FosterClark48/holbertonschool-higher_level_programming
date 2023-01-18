8821958b7dba2dc580ea6afd432a0e7be75bbed7
Foster Clark <fosterclark48@yahoo.com>
2023-01-17 21:03:38 -0600

    Added public instance method Area to get area of square

---
+++ 0x04-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square class that defines a square"""
+
+
+class Square:
+    """Passing initializing values to object"""
+    def __init__(self, size=0):
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return int(self.__size) ** 3
