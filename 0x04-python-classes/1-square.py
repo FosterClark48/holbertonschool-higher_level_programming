5f94c22978ffdb03c2a6cfadbe24f7c531e9980b
Foster Clark <fosterclark48@yahoo.com>
2023-01-17 20:52:13 -0600

    Added if statements to raise error messages

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -6,3 +6,8 @@ class Square:
     """Passing initializing values to object"""
     def __init__(self, size=0):
         self.__size = size
+        
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
