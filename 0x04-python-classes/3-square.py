2c15b9e0a4fd262356814427fc23ae9e4986a50c
Foster Clark <fosterclark48@yahoo.com>
2023-01-17 21:20:36 -0600

    Added Getter & Setter and changed args to properly match

--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -7,21 +7,18 @@ class Square:
     def __init__(self, size=0):
         self.__size = size
 
-        if not isinstance(size, int):
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
 
     def area(self):
         return int(self.__size) ** 2
-
-    @property
-    def size(self):
-        return self.size
-
-    @property.setter
-    def size(self, value):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
