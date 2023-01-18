b36bb49539e44146fb356e7bb0fb40256e606385
Foster Clark <fosterclark48@yahoo.com>
2023-01-17 21:13:48 -0600

    Added Getter & Setter

---
+++ 0x04-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Square class that defines a square"""
+
+
+class Square:
+    """Passing initializing values to object"""
+    def __init__(self, size=0):
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return int(self.__size) ** 2
+
+    @property
+    def size(self):
+        return self.size
+
+    @property.setter
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
