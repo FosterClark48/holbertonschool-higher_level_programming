b6785850fd4a8c342cf5240be2bbce724757bdde
Foster Clark <fosterclark48@yahoo.com>
2023-01-17 20:40:11 -0600

    class Square that defines a square by size

---
+++ 0x04-python-classes/1-square copy.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Square class that defines a square"""
+
+
+class Square:
+    """Passing initializing values to object"""
+    def __init__(self, size=0):
+        self.__size = size
