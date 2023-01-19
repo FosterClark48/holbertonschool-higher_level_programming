20f8bd002b57e03f662acb7376116b5ebf73b81f
Foster Clark <fosterclark48@yahoo.com>
2023-01-18 18:21:39 -0600

    adding positional args

--- 0x03-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x03-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
         for key, value in sorted(a_dictionary.items()):
-            print("{}: {}".format(one=key, two=value))
+            print("{one}: {two}".format(one=key, two=value))
