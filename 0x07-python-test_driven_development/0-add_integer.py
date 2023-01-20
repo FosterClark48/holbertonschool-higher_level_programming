6b567f8ce72c770fda6abab2298c6ade545f2e50
FosterClark48 <fosterclark48@yahoo.com>
2023-01-20 12:11:46 -0800

    put spaces between first and last char on docstrings?

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""This is the add_integer module."""
+""" This is the add_integer module """
 
 
 def add_integer(a, b=98):
-    """Function to add 2 ints"""
+    """ Function to add 2 ints """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
