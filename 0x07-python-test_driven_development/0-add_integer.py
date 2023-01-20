614cbea642a18e6c5311b26233ff6df921f66430
FosterClark48 <fosterclark48@yahoo.com>
2023-01-20 11:20:39 -0800

    removed whitespace from line 12

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,7 +9,7 @@ which adds together 2 int or float types and returns an int.
 
 def add_integer(a, b=98):
     """Function to add 2 ints"""
-    
+
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
