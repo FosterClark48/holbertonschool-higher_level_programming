982278f3e3929c538d7e5134be6ae7617dba9887
FosterClark48 <fosterclark48@yahoo.com>
2023-01-20 10:30:23 -0800

    Function that returns the sum of its two arguments as ints

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This is the add_integer module.
+
+This module supplies one function, add_integer(),
+which adds together 2 int or float types and returns an int.
+"""
+
+
+def add_integer(a, b=98):
+    """Function to add 2 ints"""
+    
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    sum = int(a) + int(b)
+    return sum
