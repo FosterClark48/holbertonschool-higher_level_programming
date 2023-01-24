78573d82cdcb1361bda0d87423c62be5ad2e8118
FosterClark48 <fosterclark48@yahoo.com>
2023-01-23 20:36:27 -0800

    Added __str__ to print out the rectangle using hashtags

---
+++ 0x06-python-more_classes/2-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Rectangle class that defines a rectangle"""
+
+
+class Rectangle:
+    """Passing initializing values to object"""
+    def __init__(self, width=0, height=0):
+
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (self.width * 2 + self.height * 2)
+
+    def __str__(self):
+        string = ""
+        if self.__width == 0 or self.__height == 0:
+            return string
+
+        for i in range(self.__height):
+            for j in range(self.__width):
+                string += "#"
+            if i < self.__height - 1:
+                string += "\n"
+        return string
