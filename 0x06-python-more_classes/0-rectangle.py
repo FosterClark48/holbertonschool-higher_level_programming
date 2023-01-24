328f9decbab9c855f87541a18535329fb63b4fd2
FosterClark48 <fosterclark48@yahoo.com>
2023-01-23 19:35:19 -0800

    Created and defined a rectangle with class Rectangle using getters and setters

---
+++ 0x06-python-more_classes/0-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Rectangle class that defines a rectangle"""
+
+
+class Rectangle:
+    """Passing initializing values to object"""
+    def __init__(self, width=0, height=0):
+        self.__height = height
+        self.__width = width
+
+    """Gets the height"""
+    @property
+    def height(self):
+        return self.__height
+
+    """Sets the height"""
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+    """Gets the width"""
+    @property
+    def width(self):
+        return self.__width
+
+    """Sets the width"""
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
