70f87385288356b8733ba95706760425fc0b9ac3
FosterClark48 <fosterclark48@yahoo.com>
2023-01-23 19:52:23 -0800

    removed docstrings above each method

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -5,33 +5,36 @@
 class Rectangle:
     """Passing initializing values to object"""
     def __init__(self, width=0, height=0):
+
         self.__height = height
         self.__width = width
 
-    """Gets the height"""
     @property
     def height(self):
+
         return self.__height
 
-    """Sets the height"""
     @height.setter
     def height(self, value):
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+
         self.__height = value
 
-    """Gets the width"""
     @property
     def width(self):
+
         return self.__width
 
-    """Sets the width"""
     @width.setter
     def width(self, value):
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+
         self.__width = value
