fae2a7f9808f7391f28969d746eb4c883ae6acea
FosterClark48 <fosterclark48@yahoo.com>
2023-01-23 19:45:57 -0800

    Shifted setter assignments to after if statements

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -16,12 +16,11 @@ class Rectangle:
     """Sets the height"""
     @height.setter
     def height(self, value):
-        self.__height = value
-
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
 
     """Gets the width"""
     @property
@@ -31,9 +30,8 @@ class Rectangle:
     """Sets the width"""
     @width.setter
     def width(self, value):
-        self.__width = value
-
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
