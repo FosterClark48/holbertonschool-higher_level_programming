6bcde9dd159b2a8ca08dc47ac46b079f39aaf398
FosterClark48 <fosterclark48@yahoo.com>
2023-01-27 14:40:26 -0800

    added docstring under print_sorted method

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
 """
 This is the my_list class and print_sorted module
-
-The my_list is a class that inherits from `list` and adds
-a `print_sorted()` method.
 """
 
 
 class MyList(list):
-    """
-    Class that extends `list` class
-    """
+    """Class that extends `list` class"""
     def __init__(self):
         super().__init__()
 
     def print_sorted(self):
+        """Prints list in ascending order"""
         print(sorted(self))
