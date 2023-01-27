f44eb2e583849e266f9da3cbad972c7653157eff
FosterClark48 <fosterclark48@yahoo.com>
2023-01-27 14:52:42 -0800

    adding everything to make sure im not missing anything

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,10 +5,10 @@ This is the my_list class and print_sorted module
 
 
 class MyList(list):
-    """Class that extends `list` class"""
+    """ Class that extends `list` class """
     def __init__(self):
         super().__init__()
 
     def print_sorted(self):
-        """Prints list in ascending order"""
+        """ Prints list in ascending order """
         print(sorted(self))
