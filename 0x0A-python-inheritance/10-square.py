bf91c4e53da7d1db08d4611765a7770e0918eae0
FosterClark48 <fosterclark48@yahoo.com>
2023-01-28 21:32:18 -0800

    Created Square class to extend Rectangle class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This is the Square class
+
+Square class extends Rectangle class and 
+raises an Error when required. It also implements
+the area method and prints a formatted string
+"""
+
+# BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """Square class extending BaseGeometry class"""
+    def __init__(self, size):
+        """initialization of class"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
