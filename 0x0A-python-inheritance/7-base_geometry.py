f5a7d3b0ef5c1334b23baeb8e6114d3f7a401d54
FosterClark48 <fosterclark48@yahoo.com>
2023-01-28 21:52:07 -0800

    removed string formatter from replacement field

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -11,6 +11,6 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """Int validator method to check for ints """
         if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+            raise ValueError("{} must be greater than 0".format(name))
