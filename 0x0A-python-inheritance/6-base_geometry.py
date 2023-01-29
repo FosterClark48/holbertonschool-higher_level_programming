9a31b0a36e750f002afb470cc3c0a36f0f6bb06d
FosterClark48 <fosterclark48@yahoo.com>
2023-01-28 19:22:54 -0800

    Defined public instance method that raises Exception w/ a message

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is the BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """ declaring empty class """
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
