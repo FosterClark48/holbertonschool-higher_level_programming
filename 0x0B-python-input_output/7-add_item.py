a8e07c2e95cd8ac149b66a850338787300ab0a14
FosterClark48 <fosterclark48@yahoo.com>
2023-01-29 22:08:09 -0800

    Trying a different way to add all args to python list, using try except method

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -15,11 +15,13 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def add_item(args):"""add
-    s all arguments to a Python list, and then save them to a file"""
-    items = load_from_json_file("add_item.json")
-    items.extend(args)
-    save_to_json_file(items, "add_item.json")
+filename = "add_item.json"
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
 
-if __name__ == "__main__":
-    add_item(sys.argv[1:])
+for arg in sys.argv[1:]:
+    items.append(arg)
+
+save_to_json_file(items, filename)
