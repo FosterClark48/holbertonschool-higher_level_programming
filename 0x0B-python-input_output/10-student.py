033de188cb6ec01ee94bd15ce669a3f4cf94dd27
FosterClark48 <fosterclark48@yahoo.com>
2023-01-30 13:28:44 -0800

    switched order of attributes

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -11,9 +11,9 @@ representation of a Student instance
 class Student:
     """Class that defines a Student"""
     def __init__(self, first_name, last_name, age):
-        self.first_name = first_name
-        self.last_name = last_name
         self.age = age
+        self.last_name = last_name
+        self.first_name = first_name
 
     def to_json(self, attrs=None):
         """Returns dict representation of Student"""
