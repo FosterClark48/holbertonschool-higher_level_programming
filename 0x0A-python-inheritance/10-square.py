b5bc9cee90c93a6d965218e2bc2d9a822ceb52f7
FosterClark48 <fosterclark48@yahoo.com>
2023-01-29 21:06:43 -0800

    fixed and removed whitespace

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,13 +2,14 @@
 """
 This is the Square class
 
-Square class extends Rectangle class and 
+Square class extends Rectangle class and
 raises an Error when required. It also implements
 the area method and prints a formatted string
 """
 
 # BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
+
 
 class Square(Rectangle):
     """Square class extending BaseGeometry class"""
