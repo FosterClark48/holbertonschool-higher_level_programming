7f525efd7dfb9e668d366614247558b94c972fc8
FosterClark48 <fosterclark48@yahoo.com>
2023-01-30 14:34:29 -0800

    Function that returns list of lists of ints representing Pascals triangle of n rows

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This is the pascal_triangle module
+
+The pascal_triangle module returns a list of
+lists of integers representing the Pascal's triangle of n
+"""
+
+
+def pascal_triangle(n):
+    """Returns Pascals Triangle w/ 5 rows"""
+    res = [[1]]
+    for i in range(n - 1):
+        temp = [0] + res[-1] + [0]
+        row = []
+        for j in range(len(res[-1]) + 1):
+            row.append(temp[j] + temp[j+1])
+        res.append(row)
+    return res
