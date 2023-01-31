640ede4d6a39b7c616cbf95e8e36098fcf078a25
FosterClark48 <fosterclark48@yahoo.com>
2023-01-31 15:29:07 -0800

    fixed indentation on continuation line

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -92,5 +92,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """Returns str representation of Rectangle"""
-        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x,self.y, 
-                                                        self.width, self.height))
+        return ("[Rectangle] ({}) {}/{} - {}/{}"
+                .format(self.id, self.x, self.y, self.width, self.height))
