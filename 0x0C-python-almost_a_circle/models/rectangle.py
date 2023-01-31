99e8c44ca853c747e97137dff5056b167b8cf756
FosterClark48 <fosterclark48@yahoo.com>
2023-01-31 13:57:45 -0800

    shortened class docstring

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
 This is the Rectangle class
-
-Rectangle class extends Base class
 """
 
 
@@ -13,48 +11,48 @@ class Rectangle(Base):
     """Rectangle class extending Base class"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """Private initialization of class"""
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
     def width(self):
         """returns value of attribute, width"""
-        return self.width
+        return self.__width
 
     @width.setter
     def size(self, value):
         """sets value of attribute"""
-        self.width = value
+        self.__width = value
 
     @property
     def height(self):
         """returns value of attribute, height"""
-        return self.height
+        return self.__height
 
     @height.setter
     def size(self, value):
         """sets value of attribute"""
-        self.height = value
+        self.__height = value
 
     @property
     def x(self):
         """returns value of attribute, x"""
-        return self.x
+        return self.__x
 
     @x.setter
     def size(self, value):
         """Private setter for x"""
-        self.x = value
+        self.__x = value
 
     @property
     def y(self):
         """return value of attribute, y"""
-        return self.y
+        return self.__y
 
     @y.setter
     def size(self, value):
         """sets value of attribute"""
-        self.y = value
+        self.__y = value
