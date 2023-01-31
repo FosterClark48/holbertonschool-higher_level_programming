02ebd72ea7de4451204349824e7c15e605ed26ec
FosterClark48 <fosterclark48@yahoo.com>
2023-01-31 11:46:57 -0800

    removed init attr

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,9 +5,9 @@ This is the my_list class and print_sorted module
 
 
 class MyList(list):
-    """ Class that extends `list` class """
-    def __init__(self):
-        super().__init__()
+    """ MyList class that extends `list` """
+    # def __init__(self):
+    #     super().__init__()
 
     def print_sorted(self):
         """ Prints list in ascending order """
