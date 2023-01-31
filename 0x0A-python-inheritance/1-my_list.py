520eeb567d5894fd1c776050f2637690b71d4a20
FosterClark48 <fosterclark48@yahoo.com>
2023-01-31 12:09:11 -0800

    uncommented the init lines

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,8 +6,8 @@ This is the my_list class and print_sorted module
 
 class MyList(list):
     """ MyList class that extends `list` """
-    # def __init__(self):
-    #     super().__init__()
+    def __init__(self):
+        super().__init__()
 
     def print_sorted(self):
         """ Prints list in ascending order """
