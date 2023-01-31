9cc773d82a77006f72e9c13eca817a45569c881a
FosterClark48 <fosterclark48@yahoo.com>
2023-01-31 12:44:58 -0800

    made docstring for module shorter

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 """
 This is the inherits_from Module
-
-The inherits_from module returns True if the object is
-an instance of a class that inherited (directly or indirectly)
-from the specified class; otherwise False.
 """
 
 
