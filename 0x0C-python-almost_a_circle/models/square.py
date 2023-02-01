ec27b2fcc64244cf9e3f3102a23d16e483cd9c7e
FosterClark48 <fosterclark48@yahoo.com>
2023-01-31 20:25:32 -0800

    Created class Square that inherits from Rectangle, changing size for width and height

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This is the Square class
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class extending Rectangle class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Class Constructor"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """returns value of attribute, size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """sets value of attribute"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Returns str representation of Rectangle"""
+        return ("[Square] ({}) {}/{} - {}"
+                .format(self.id, self.x, self.y, self.width))
+
+    def update(self, *args, **kwargs):
+        """Assign an arg to each attribute"""
+        if args:
+            attrs = ["id", "size", "x", "y"]
+            for i in range(len(args)):
+                setattr(self, attrs[i], args[i])
+        for key, val in kwargs.items():
+            setattr(self, key, val)
