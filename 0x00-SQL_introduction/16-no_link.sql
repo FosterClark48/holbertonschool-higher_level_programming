206a47643ccf416b80b7356f03dbe2e8cc42d5e7
FosterClark48 <fosterclark48@yahoo.com>
2023-02-13 13:56:38 -0800

    changed SELECT statement from COUNT(*) to score, name

--- 0x00-SQL_introduction/16-no_link.sql
+++ 0x00-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 -- script that lists all records of the table second_table without rows that don't have a name value.
-SELECT COUNT(*)
+SELECT score, name
     FROM second_table
     WHERE name IS NOT NULL AND name <> ''
     ORDER BY score DESC;
