99fc7340f081b9524d5b5b401a1641a7c28badea
FosterClark48 <fosterclark48@yahoo.com>
2023-02-13 12:13:25 -0800

    had to change quotes to backticks

--- 0x00-SQL_introduction/8-count_89.sql
+++ 0x00-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 -- script that displays the number of records with id = 89 in the table first_table.
-SELECT COUNT(id) FROM first_table;
+SELECT COUNT(*) FROM first_table WHERE `id`=89;
