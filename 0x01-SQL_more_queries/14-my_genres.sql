144b72cabd4228bc54402df0abc9a14424879d26
FosterClark48 <fosterclark48@yahoo.com>
2023-02-14 08:48:39 -0800

    changed join to inner join

--- 0x01-SQL_more_queries/14-my_genres.sql
+++ 0x01-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 -- script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
-SELECT tv_genres.name
-    FROM tv_show_genres JOIN tv_genres
+SELECT name
+    FROM tv_show_genres INNER JOIN tv_genres
     ON tv_genres.id = tv_show_genres.genre_id
     JOIN tv_shows
     ON tv_shows.id = tv_show_genres.show_id
