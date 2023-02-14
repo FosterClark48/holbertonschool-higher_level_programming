3dcf0bafb58d4302dac4e0df20fdeee1b5a4f21d
FosterClark48 <fosterclark48@yahoo.com>
2023-02-13 20:04:36 -0800

    Script that creates a db and user w/ SELECT privileges and pw if db and user dont exist

---
+++ 0x01-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+-- script that creates the database hbtn_0d_2 and the user user_0d_2 
+-- w/ select privileges and a password if the database and user dont exist.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_1'@'localhost';
