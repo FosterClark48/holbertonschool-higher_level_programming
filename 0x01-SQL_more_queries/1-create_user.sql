464c93c571d201a08fe83b7b6ee06890c856d45c
FosterClark48 <fosterclark48@yahoo.com>
2023-02-13 19:55:05 -0800

    Script that creates a server user w/ all privileges and a password if the user doesnt exist

---
+++ 0x01-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+-- script that creates the MySQL server user user_0d_1 w/ all privileges and a password.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
