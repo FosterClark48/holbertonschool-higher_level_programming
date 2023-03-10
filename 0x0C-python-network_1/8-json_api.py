77b35711dce46e9d74e620e83611e61919d93f68
FosterClark48 <fosterclark48@yahoo.com>
2023-03-10 11:36:37 -0800

    Added if __name == "__main__" to ensure script is only ran directly and not when imported

--- 0x0C-python-network_1/8-json_api.py
+++ 0x0C-python-network_1/8-json_api.py
@@ -6,30 +6,32 @@ to http://0.0.0.0:5000/search_user with the letter as a parameter.
 import requests
 import sys
 
-"""
-Check if one command line argument was provided
-and set it as the value of q
-"""
-if len(sys.argv) == 2:
-    q = sys.argv[1]
-else:  # set q to an empty string
-    q = ""
 
-"""
-Send a POST request to the search_user endpoint
-with the q parameter set to the value of q
-"""
-response = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
+if __name__ == "__main__":
+    """
+    Check if one command line argument was provided
+    and set it as the value of q
+    """
+    if len(sys.argv) == 2:
+        q = sys.argv[1]
+    else:  # set q to an empty string
+        q = ""
 
-try:
-    """Try and parse the response body as JSON"""
-    data = response.json()
-    if data:
-        """If JSON object exists, print id and name in desired format"""
-        print("[{}] {}".format(data["id"], data["name"]))
-    else:
-        """If JSON object is empty, print 'No result'"""
-        print("No result")
-except ValueError:
-    """if parsing fails, print 'Not a valid JSON'"""
-    print("Not a valid JSON")
+    """
+    Send a POST request to the search_user endpoint
+    with the q parameter set to the value of q
+    """
+    response = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
+
+    try:
+        """Try and parse the response body as JSON"""
+        data = response.json()
+        if data:
+            """If JSON object exists, print id and name in desired format"""
+            print("[{}] {}".format(data["id"], data["name"]))
+        else:
+            """If JSON object is empty, print 'No result'"""
+            print("No result")
+    except ValueError:
+        """if parsing fails, print 'Not a valid JSON'"""
+        print("Not a valid JSON")
