c5f669a11daedd4f24b14b87b69b35bd51a168b7
FosterClark48 <fosterclark48@yahoo.com>
2023-03-10 10:45:21 -0800

    removed with context manager

--- 0x0C-python-network_1/5-hbtn_header.py
+++ 0x0C-python-network_1/5-hbtn_header.py
@@ -12,13 +12,12 @@ if __name__ == '__main__':
     """Retrieve URL argument passed to script"""
     url = sys.argv[1]
     """
-    Ensure response is properly closed.
     Open URL with requests.get() and store in response variable
     """
-    with requests.get(url) as response:
-        """
-        Use headers property of response object to access headers.
-        Use get() method to retrieve value of X-Request-Id.
-        Print X-Request-Id value
-        """
-        print(response.headers.get('X-Request-Id'))
+    response = requests.get(url)
+    """
+    Use headers property of response object to access headers.
+    Use get() method to retrieve value of X-Request-Id.
+    Print X-Request-Id value
+    """
+    print(response.headers.get('X-Request-Id'))
