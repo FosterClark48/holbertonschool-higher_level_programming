5ed301bfd841959e1eeb8213bf7c8fbffaca340e
FosterClark48 <fosterclark48@yahoo.com>
2023-04-07 20:23:22 -0700

    Script that prints 3 lines: "C is fun", "Python is cool", "JavaScript is amazing"

---
+++ 0x00-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const mommaJava = 'JavaScript is amazing';
+const papaC = 'C is fun';
+const brotherPython = 'Python is cool';
+
+console.log(papaC);
+console.log(brotherPython);
+console.log(mommaJava);
