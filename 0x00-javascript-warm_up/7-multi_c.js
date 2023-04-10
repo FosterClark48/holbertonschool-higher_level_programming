9cb0892de6593582a9f832d8fbffb3f55267faf6
FosterClark48 <fosterclark48@yahoo.com>
2023-04-10 11:52:31 -0700

    fixed semistandard styling issues - changed let x to const x, added semicolon to line 7

--- 0x00-javascript-warm_up/7-multi_c.js
+++ 0x00-javascript-warm_up/7-multi_c.js
@@ -4,9 +4,9 @@ const args = process.argv.slice(2);
 
 // if args = 0 or is Not a Number, print message
 if (args.length === 0 || isNaN(parseInt(args[0]))) {
-  console.log('Missing number of occurences')
+  console.log('Missing number of occurences');
 } else {
-  let x = parseInt(args[0]);
+  const x = parseInt(args[0]);
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
