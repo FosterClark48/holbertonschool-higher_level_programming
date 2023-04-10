f40e1ef4d7462109b442fd78f0f6b6995a089b74
FosterClark48 <fosterclark48@yahoo.com>
2023-04-10 11:23:28 -0700

    script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop

---
+++ 0x00-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Create array to store phrases
+const phrases = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+// For each element in phrases, assign value of element to phrase & print
+for (const phrase of phrases) {
+  console.log(phrase);
+}
