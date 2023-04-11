d9cfa76c4825fad8c0993f8b77671373bb093fa3
FosterClark48 <fosterclark48@yahoo.com>
2023-04-11 10:38:22 -0700

    Created class Rectangle that has width & height attributes, if w & h arent positive ints then return empty obj. Added print() method that prints a rectangle w/ the char X

---
+++ 0x01-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+// Create class Rectangle that has width & height attributes
+// if w & h are not positive ints, return empty obj
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // Create print() method which prints a rectangle w/ the char X
+  print () {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Rectangle;
