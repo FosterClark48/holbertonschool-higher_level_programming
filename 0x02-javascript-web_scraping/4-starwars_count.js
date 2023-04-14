ad6bfbf176485ebcf4f9776a56e73b5b02b53a56
FosterClark48 <fosterclark48@yahoo.com>
2023-04-14 12:06:48 -0700

    removed logging the error, changed it to if no error, then..

--- 0x02-javascript-web_scraping/4-starwars_count.js
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -10,20 +10,17 @@ const wedgeID = '18';
 
 // Make a GET request to the apiURL using request with a callback function that handles the response
 request(apiURL, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    process.exit(1);
+  if (!error) {
+    // Parse JSON response recieved in the body variable. .results property of parsed object is an array of movies
+    const movies = JSON.parse(body).results;
+    // movies.filter() iterates over the movies array & returns a new array containing only the movies where Wedge Antilles is present
+    // filter() method takes a callback function as an argument, which is invoked once for each element in the array.
+    // The arrow function takes one argument, movie, which represents the current element being processed in the movies array.
+    // The includes() function checks if the characters property of the movie object includes the URL for Wedge Antilles.
+    // It checks if the specified value (the URL for Wedge Antilles) is present in the array
+    // After the filter() function is done iterating over the movies array, it returns a new array containing only the movies where the condition in the arrow function is true.
+    // By calling the .length property on the array it gives us the count of movies featuring Wedge Antilles.
+    const numMoviesWithWedge = movies.filter(movie => movie.characters.includes(`https://swapi-api.hbtn.io/api/people/${wedgeID}/`)).length;
+    console.log(numMoviesWithWedge);
   }
-
-  // Parse JSON response recieved in the body variable. .results property of parsed object is an array of movies
-  const movies = JSON.parse(body).results;
-  // movies.filter() iterates over the movies array & returns a new array containing only the movies where Wedge Antilles is present
-  // filter() method takes a callback function as an argument, which is invoked once for each element in the array.
-  // The arrow function takes one argument, movie, which represents the current element being processed in the movies array.
-  // The includes() function checks if the characters property of the movie object includes the URL for Wedge Antilles.
-  // It checks if the specified value (the URL for Wedge Antilles) is present in the array
-  // After the filter() function is done iterating over the movies array, it returns a new array containing only the movies where the condition in the arrow function is true.
-  // By calling the .length property on the array it gives us the count of movies featuring Wedge Antilles.
-  const numMoviesWithWedge = movies.filter(movie => movie.characters.includes(`https://swapi-api.hbtn.io/api/people/${wedgeID}/`)).length;
-  console.log(numMoviesWithWedge);
 });
