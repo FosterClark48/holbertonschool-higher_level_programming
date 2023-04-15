0ad9aa8e9cd4be9312ec179351df35b9e710b34c
Foster Clark <fosterclark48@yahoo.com>
2023-04-15 12:12:44 -0500

    JavaScript script that adds the class red to the <header> element when the user clicks on the tag DIV#red_header using JQuery API.

---
+++ 0x03-javascript-web_jquery/3-script.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Write a JavaScript script that adds the class red to the <header> element
+// when the user clicks on the tag DIV#red_header using JQuery API.
+
+// Ensure that the script will only run after the DOM has fully loaded.
+$(document).ready(function () {
+  // Select div element using id, add click event listener
+  $('#red_header').on('click', function() {
+    // Execute this code when click event occurs, turning the header red
+    $('header').addClass('red');
+  });
+});
